82ec134d563fc821cf52f63080a9a16472fcd45d
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-04-09 13:36:25 +0530

    added models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Users(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    usertype = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='pics/')
+    address = models.CharField(max_length=500)
+    

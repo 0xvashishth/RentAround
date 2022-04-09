44d3912d4f00ab9c60cb7fd5d20eacd5ce6d1f1a
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-04-09 21:32:09 +0530

    my commit vashishth

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     # path('get_name', views.get_name, name='get_name'),
-    path('index', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('', views.index, name='index'),
-    path('login', views.login, name='login'),
-
+    path('login/', views.login, name='login'),
+    path('register', views.register_user_page, name='userregister'),
+    path('register_user', views.register_user, name='user_register')
 ]
--- base/views.py
+++ base/views.py
@@ -23,3 +23,39 @@ def login(request):
             print(user)
         return redirect('login')
     return render(request, 'login.html')
+
+
+
+
+
+
+
+
+
+def register_user_page(request):
+    return render(request,'register.html')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        usertype = request.POST.get('usertype')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        country = request.POST.get('country')
+        address = request.POST.get('address')
+        img = request.FILES.get('img')
+
+        if(User.objects.filter(username = username).exists()):
+            messages.info(request,"username is not available")
+			return redirect('register')
+
+
+        print(fname)
+
+        return render(request,'register.html')

beedd81b051fffd18b541de81e9daa4e2dcecd5d
Smit <smitpadaliya5@gmail.com>
2022-04-09 21:38:31 +0530

    my commit

--- base/urls.py
+++ base/urls.py
@@ -2,16 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('get_name', views.get_name, name='get_name'),
     path('index/', views.index, name='index'),
     path('', views.index, name='index'),
-<<<<<<< HEAD
-    path('login/', views.login, name='login'),
+    # path('login/', views.login, name='login'),
     path('register', views.register_user_page, name='userregister'),
-    path('register_user', views.register_user, name='user_register')
-=======
+    path('register_user', views.register_user, name='user_register'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
 
->>>>>>> e9eceda188278e8f6fab73dd02fee70a8090d1bc
 ]

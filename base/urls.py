9133923b2a59408a31478da6649ac7cc45c051c4
Ayush Rudani <ayushcrudani@gmail.com>
2022-04-09 17:19:57 +0530

    Update

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('get_name', views.get_name, name='get_name'),
+    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
+
 ]

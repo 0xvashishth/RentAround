c4b540462fe33121c131f3e79626a527d819bcf9
Smit <smitpadaliya5@gmail.com>
2022-04-10 21:26:44 +0530

    css commit

--- base/views.py
+++ base/views.py
@@ -117,9 +117,8 @@ def house_list(request):
     # print(users)
     # user = User.objects.filter(id__in=users).values_list('id', flat=True)
     # print(user)
-
-    if homeList is not None:
-        return render(request, 'house_list.html', {'homeList': homeList, })
+        if homeList is not None:
+            return render(request, 'house_list.html', {'homeList': homeList, })
     return redirect('login')
 
 
